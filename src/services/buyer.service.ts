7d3b21c16f5e7695ff7abbcc7d6f18150ed6802e
Le Duc Thai <leducthai2001@gmail.com>
2024-03-08 01:54:56 +0700

    handle buyer service UPDATE methods

--- src/services/buyer.service.ts
+++ src/services/buyer.service.ts
@@ -23,3 +23,31 @@ export async function createBuyer(buyerData: IBuyerDocument) {
     await BuyerModel.create(buyerData);
   }
 }
+
+export async function updateBuyerIsSellerProp(email: string) {
+  await BuyerModel.updateOne(
+    { email },
+    {
+      $set: {
+        isSeller: true
+      }
+    }
+  ).exec();
+}
+
+export async function updateBuyerPurchasedGigsProp(buyerId: string, purchasedGigId: string, type: string) {
+  await BuyerModel.updateOne(
+    { _id: buyerId },
+    type === 'purchased-gigs'
+      ? {
+          $push: {
+            purchasedGigs: purchasedGigId
+          }
+        }
+      : {
+          $pull: {
+            purchasedGigs: purchasedGigId
+          }
+        }
+  ).exec();
+}
